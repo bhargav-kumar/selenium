eb2e1e24edad63a3f9b3b00ac9c1664aaad46363
bhargav-kumar <ch.bhargav_kumar@hotmail.com>
2021-12-23 12:53:51 +0530

    Added dec 23 classes

---
+++ src/javaBasics/ChildClass.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+public class ChildClass extends ParentClass {
+
+	public static void main(String[] args) {
+
+		ChildClass childClass = new ChildClass();
+
+		ParentClass childClass1 = new ChildClass();
+//		ChildClass childClass2 = new ParentClass();
+		childClass.sum();
+		childClass.substraction();
+	}
+
+}
---
+++ src/javaBasics/DynamicPolymorphismTest.java
@@ -0,0 +1,15 @@
+package javaBasics;
+
+public class DynamicPolymorphismTest {
+
+	
+	public void bank(String bank) {
+		System.out.println("bank in DynamicPolymorphismTest class");
+	}
+	
+	public static void main(String[] args) {
+		DynamicPolymorphismTest dp = new DynamicPolymorphismTest();
+//		dp.bank("");
+	}
+
+}
