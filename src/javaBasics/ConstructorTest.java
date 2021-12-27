9c44fac095bb1091faacf4b31cbe2ebb89d5d3a5
bhargav-kumar <ch.bhargav_kumar@hotmail.com>
2021-12-27 09:02:52 +0530

    Added dec 27 class

---
+++ src/javaBasics/ConstructorTest.java
@@ -0,0 +1,40 @@
+package javaBasics;
+
+public class ConstructorTest {
+
+	
+	//No-argument constructor
+	public ConstructorTest() {
+		System.out.println("ConstructorTest");
+	}
+	
+	//argument constructor
+	public ConstructorTest(String str) {
+		System.out.println("ConstructorTest with arguments");
+	}
+
+	public ConstructorTest(String name, String designation, int age) {
+		System.out.println("ConstructorTest with multiple arguments");
+	}
+	
+	static {
+		System.out.println("Static block");
+	}
+	
+	
+	public void testMethod() {
+		System.out.println("Hello");
+	}
+	
+	
+	public static void main(String[] args) {
+			
+		ConstructorTest ct = new ConstructorTest();
+		ct.testMethod();
+		
+		ConstructorTest ct1 = new ConstructorTest("Peter", "Test Engineer", 50);
+		ct1.testMethod();
+		
+	}
+
+}
