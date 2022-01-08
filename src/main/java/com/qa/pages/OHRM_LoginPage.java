ab450b33b8d5e2701d7630dd532dc8e7d9df0b93
bhargav-kumar <ch.bhargav_kumar@hotmail.com>
2022-01-08 13:16:49 +0530

    Added jan 8 classes

--- src/main/java/com/qa/pages/OHRM_LoginPage.java
+++ src/main/java/com/qa/pages/OHRM_LoginPage.java
@@ -2,22 +2,28 @@ package com.qa.pages;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.log4testng.Logger;
 
 import com.qa.baseSetup.TestBase;
 import com.qa.locators.LoginLocators;
 
 public class OHRM_LoginPage extends TestBase {
 
+	Logger log = Logger.getLogger(OHRM_LoginPage.class);
+	
 	public void enterUserName(String userName) {
 		driver.findElement(LoginLocators.USERNAME_TEXTBOX).sendKeys(userName);
+		log.info("Entered username - "+ userName);
 	}
 
 	public void enterPassword(String password) {
 		driver.findElement(LoginLocators.PASSWORD_TEXTBOX).sendKeys(password);
+		log.info("Entered password - "+ password);
 	}
 
 	public void clickOnLoginButton() {
 		driver.findElement(LoginLocators.LOGIN_BUTTON).click();
+		log.info("Login button clicked");
 	}
 	
 	public boolean homePageLogo() {
--- src/test/java/com/qa/tests/OHRM_LoginTest.java
+++ src/test/java/com/qa/tests/OHRM_LoginTest.java
@@ -1,24 +1,30 @@
 package com.qa.tests;
 
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+//import org.testng.log4testng.Logger;
+import org.apache.log4j.Logger;
 
 import com.qa.baseSetup.TestBase;
 import com.qa.pages.OHRM_LoginPage;
 
 public class OHRM_LoginTest extends TestBase{
 
+	Logger log = Logger.getLogger(OHRM_LoginTest.class);
 	OHRM_LoginPage ohrmLogin = new OHRM_LoginPage();
 	
 	@BeforeTest
 	public void setup() {
 		TestBase.intialization();
+		log.info("Launched application");
 	}
 
 	@Test
 	public void testMethod() {
 		ohrmLogin.logIntoApplication();
+		log.info("User logged into the application");
 	}
 
 	@AfterTest
